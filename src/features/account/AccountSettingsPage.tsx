047c2c673faab283e53081fd4285be1eb12c8f4a
cometstone <80153517@qq.com>
2026-08-26 20:08:25 +0800

    style: round account settings panel

--- src/features/account/AccountSettingsPage.tsx
+++ src/features/account/AccountSettingsPage.tsx
@@ -85,7 +85,10 @@ export function AccountSettingsPage() {
 		<Flex
 			style={{
 				background: token.colorBgContainer,
+				border: `${token.lineWidth}px ${token.lineType} ${token.colorBorderSecondary}`,
+				borderRadius: 8,
 				minHeight: 520,
+				overflow: "hidden",
 				width: "100%",
 			}}
 			vertical={compact}
